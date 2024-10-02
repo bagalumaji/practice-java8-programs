e5c38124f2ef09a05f3741f10bf8414850bed1f1
umaji <bagalumaji@gmail.com>
2024-10-02 11:16:07 +0530

    Comparator Demo

--- lambda/ComparatorDemo.java
+++ lambda/ComparatorDemo.java
@@ -1,7 +1,34 @@
 package lambda;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class ComparatorDemo {
     public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 1, 4, 5, 6, 3);
+        sortUsingLambda(list);
+    }
 
+    public static void sortUsingLambda(List<Integer> list) {
+        Collections.sort(list,(a, b) -> b - a);
+        System.out.println(list);
+    }
+
+    public static void sortList(List<Integer> list) {
+        Collections.sort(list);
+        System.out.println(list);
+    }
+
+    public static void sortListInDescendingOrder(List<Integer> list) {
+        Collections.sort(list, new ComparatorImpl());
+        System.out.println(list);
+    }
+}
+
+class ComparatorImpl implements Comparator<Integer> {
+    public int compare(Integer a, Integer b) {
+        return b - a;
     }
 }
