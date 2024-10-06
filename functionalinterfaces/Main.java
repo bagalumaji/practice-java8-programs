88d3eee1127859c54155e7eaef8ba23766127636
umaji <bagalumaji@gmail.com>
2024-10-06 10:42:48 +0530

    created demo class

---
+++ functionalinterfaces/Main.java
@@ -0,0 +1,21 @@
+package functionalinterfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        IPrint print=new ConsolePrinterImpl();
+        print.print();
+
+        //using inner class
+        IPrint print1=new IPrint() {
+            @Override
+            public void print() {
+                System.out.println("inner class way");
+            }
+        };
+        print1.print();
+
+        //using lambda
+        IPrint print2=()->System.out.println("lambda way");
+        print2.print();
+    }
+}
